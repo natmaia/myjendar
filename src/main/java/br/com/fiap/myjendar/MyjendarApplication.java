bd483186780ec30a93ea51f4de091c41da1260aa
Nathalia Maia <devmaia@outlook.com>
2023-10-09 21:45:07 -0300

    🚀 Started MVC with Thymeleaf - Dependencies ⚙️

---
+++ src/main/java/br/com/fiap/myjendar/MyjendarApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.myjendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyjendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/myjendar/MyjendarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.myjendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyjendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
